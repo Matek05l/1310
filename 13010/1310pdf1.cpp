e70c764338e33d15f7854875fa8195ad549c7597
Mateusz Lig <snowdecember263@gmail.com>
2025-10-14 10:50:58 +0200

    Dodaj pliki projektów.

---
+++ 13010/1310pdf1.cpp
@@ -0,0 +1,12 @@
+//                  ZADANIE1
+#include <stdio.h>
+
+int main() {
+   printf("  MLMLML        ML\n");
+    printf("    ML         ML\n");
+    printf("    ML         ML\n");
+    printf("    ML         ML\n");
+    printf("    ML         ML\n");
+    printf("    ML         ML\n");
+    return 0;
+}
