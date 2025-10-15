397a996de5cc2c3e0e674ac4b8b6ca1548e83540
Mateusz Lig <snowdecember263@gmail.com>
2025-10-15 17:11:16 +0200

    aa

--- 13010/1310pdf1.cpp
+++ 13010/1310pdf1.cpp
@@ -1,12 +1,83 @@
-//                  ZADANIE1
-#include <stdio.h>
+// ZADANIE1
 
-int main() {
-   printf("  MLMLML        ML\n");
-    printf("    ML         ML\n");
-    printf("    ML         ML\n");
-    printf("    ML         ML\n");
-    printf("    ML         ML\n");
-    printf("    ML         ML\n");
-    return 0;
-}
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+  //  cout << "MLMLMLML         ML \n";
+    //cout << "  ML             ML \n ";
+    //cout << " ML             ML \n";
+    //cout << "  ML             ML \n";
+    //cout << "  ML             ML \n";
+    //cout << "  ML             ML \n";
+    //return 0;
+//}
+
+
+// ZADANIE 2
+
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+  //  cout << "                *\n";
+  //  cout << "               ***\n";
+  //  cout << "              *****\n";
+  //  cout << "             *******\n";
+  //  cout << "            *********\n";
+  //  cout << "**********************************\n";
+  //  cout << "   ****************************\n";
+  //  cout << "     ************************\n";
+  //  cout << "       ********************\n";
+  //  cout << "        ******************\n";
+  //  cout << "       ********************\n";
+  //  cout << "      *********** **********\n";
+  //  cout << "     *******          *******\n";
+  //  cout << "    *****                *****\n";
+  //  cout << "   ***                      ***\n";
+  //  cout << "  *                            *\n";
+  //  return 0;
+//}
+
+// ZADANIE 3
+
+//#include <iostream>
+//using namespace std;
+
+//int main()
+//{
+//    cout << "              Lista Obecnoœci \n";
+//    cout << "nr.         imie           nazwisko \n";
+//    cout << "1.          Mateusz        Ligocki \n";
+//    cout << "2.          Adam           Majda \n";
+//   cout << "3.           Dorian         Sztamblewski";
+//}
+
+// ZADANIE 4
+//#include <iostream>
+//using namespace std;
+//int main() {
+//    int a;
+//    int b; 
+//    int c;
+//    cout << "podaj liczbe \n";
+//    cin >> a;
+//    cout << "podaj drug¹ liczbe \n";
+//    cin >> b;
+//    c = a * b;
+//    cout << c;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+//    float a;
+//    float b; 
+//    float c;
+//    cout << "podaj liczbe \n";
+//    cin >> a;
+//    cout << "podaj drug¹ liczbe \n";
+//    cin >> b;
+//    c = a * b;
+//    cout << c;
+//}
