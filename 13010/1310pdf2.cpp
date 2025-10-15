eaa28801e7906000dbfbf566d37985ef71887c11
Mateusz Lig <snowdecember263@gmail.com>
2025-10-15 18:17:23 +0200

    pętle

--- 13010/1310pdf2.cpp
+++ 13010/1310pdf2.cpp
@@ -1,4 +1,38 @@
-// Zadanie 1
+// Dzia³anie pêtli for
 
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	for (int i = 0; i < 10; i++ ) {
+//		cout << i << "\n";
+//	}
+//	return 0;
+//}
+
+// Dzia³anie pêtli while 
+
+//#include <iostream>
+//using namespace std; 
+//int main() {
+//	int i = 0;
+//	while(i < 10){
+//		cout << i << "\n";
+//		i++;
+//	}
+//	return 0;
+//}
+
+// Dzia³nie pêtli do while
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int i = 0;
+//	do {
+//		cout << i << "\n";
+//		i++;
+//	} while (i < 10);
+//	return 0;
+//}
+
+// instrukcje warunkowe 
